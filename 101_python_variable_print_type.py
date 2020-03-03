7c216897440e75959a90eb6385aec694c2c97b3d
Kevin Monteiro <kevin.monteiro96@gmail.com>
2020-03-03 11:10:54 +0000

    variables, prints and types

---
+++ 101_python_variable_print_type.py
@@ -0,0 +1,15 @@
+# Variables
+# like a box. Give it a name, put stuff inside
+book = 'Rich dad poor dad'
+
+# print function
+book
+print(book)
+
+# type function
+# allows us to check data types
+data_type_of_book = type(book)
+print(data_type_of_book)
+
+## Conventions
+#lower case for variable naming with under score
